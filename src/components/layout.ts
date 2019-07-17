d539ed712dd143dbcd1ec1398aa5c93278e29bec
Erik Vullings <erik.vullings@gmail.com>
2019-07-17 19:13:27 +0200

    Added logo + title, display active lesson

--- src/components/layout.ts
+++ src/components/layout.ts
@@ -2,7 +2,9 @@ import m, { Vnode } from 'mithril';
 import logo from '../assets/logo_white.svg';
 import { dashboardSvc } from '../services/dashboard-service';
 
-const isActive = (path: string) => (m.route.get().indexOf(path) >= 0 ? '.active' : '');
+const stripRouteParams = (path: string) => path.replace(/:[a-zA-Z]+/, '');
+
+const isActive = (path: string) => (m.route.get().indexOf(stripRouteParams(path)) >= 0 ? '.active' : '');
 
 export const Layout = () => ({
   view: (vnode: Vnode) =>
@@ -10,23 +12,30 @@ export const Layout = () => ({
       m(
         'nav',
         m('.nav-wrapper', [
-          m(
-            'a.brand-logo[href=#]',
-            { style: 'margin-left: 20px' },
-            m(`img[width=48][height=48][src=${logo}]`, { style: 'margin-top: 5px; margin-left: -5px;' })
-          ),
+          m('a.brand-logo[href=#]', { style: 'margin-left: 20px' }, [
+            m(`img[width=48][height=48][src=${logo}]`, { style: 'margin-top: 5px; margin-left: -5px;' }),
+            m(
+              'div',
+              { style: 'margin-top: 0px; position: absolute; top: 16px; left: 50px; width: 400px;' },
+              m(
+                'h4.center.orange.darken-3.hide-on-med-and-down',
+                { style: 'text-align: left; margin: 0;' },
+                'Lessons Learned Library'
+              )
+            ),
+          ]),
           m(
             'ul.right',
             dashboardSvc
               .getList()
-              .filter(d => d.visible)
+              .filter(d => d.visible || isActive(d.route))
               .map(d =>
                 m(
                   `li${isActive(d.route)}`,
                   m(
-                    'a',
-                    { href: d.route, oncreate: m.route.link },
-                    m('i.material-icons.right', d.icon ? m('i.material-icons', d.icon) : d.title)
+                    m.route.Link,
+                    { href: d.route },
+                    m('span', d.icon ? m('i.material-icons.white-text', d.icon) : d.title)
                   )
                 )
               )
