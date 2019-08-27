dba8702fce514c82f9d69b0a36925d870077ef6a
Erik Vullings <erik.vullings@gmail.com>
2019-08-27 20:40:55 +0200

    Added some about text

--- src/components/about/about-page.ts
+++ src/components/about/about-page.ts
@@ -1,10 +1,15 @@
 import m from 'mithril';
+import { SlimdownView } from 'mithril-ui-form';
+
+const md = `#### About the Lessons Learned Library (L3)
+
+The Lessons Learned Library was created by Dirk Stolk and Erik Vullings from [TNO](https://wwww.tno.nl) as part of the DRIVER+ project.
+
+The source code is publicly available on [GitHub](https://github.com/DRIVER-EU/lessons-learned-framework).
+
+This project has received funding from the European Union's 7th Framework Programme for Research, Technological Development and Demonstration under Grant Agreement (GA) N°#607798.`;
 
 export const AboutPage = () => ({
   view: () =>
-    m('.row', [
-      m('h1', 'About'),
-      m('h1', 'Attribution'),
-      m('ul.collection', [m('li.collection-item', 'Logo: Group class by Fiki Ahmadi from the Noun Project.')]),
-    ]),
+    m('.row', m(SlimdownView, { md })),
 });
