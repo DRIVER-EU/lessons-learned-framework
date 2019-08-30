bb5a5d50c62f4b9cfb2adb34c0fd0a8647146098
Erik Vullings <erik.vullings@gmail.com>
2019-08-30 16:59:49 +0200

    Added version and D+ link

--- src/components/home/home-page.ts
+++ src/components/home/home-page.ts
@@ -76,12 +76,18 @@ export const HomePage = () => ({
     ]),
     m(
       'footer.page-footer.yellow.darken-3',
-      { style: 'heigth: 75px; padding: 5px 0;' },
+      { style: 'height: 100px; padding: 5px 0;' },
       m(
         '.container',
         m('.clearfix', [
-          m('img', { src: euLogo, width: 100, height: 67, style: 'float: left; margin-right: 10px;' }),
-          m('img', { src: driverLogo, width: 67, height: 67, style: 'float: right; margin-left: 10px;' }),
+          m('div', { style: 'float: left; margin-right: 10px;' }, [
+            m('img', { src: euLogo, width: 100, height: 67, style: 'display: block; margin-left: 20px;' }),
+            m('span', 'v1.0, September 2019'),
+          ]),
+          m('div', { style: 'float: right; margin-left: 10px;' }, [
+            m('img', { src: driverLogo, width: 67, height: 67, style: 'display: block; margin-left: 40px;' }),
+            m('a.primary-text', { style: 'display: block', href: 'https://www.project-driver.eu', target: '_blank' }, 'www.project-driver.eu'),
+          ]),
           m(
             '.white-text',
             `This project has received funding from the European Union's 7th Framework Programme for Research,
