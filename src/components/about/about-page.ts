40cac9dae7ad160746c898b434c406386e0bd669
Erik Vullings <erik.vullings@gmail.com>
2019-11-06 18:16:27 +0100

    Updated documentation and manual

--- src/components/about/about-page.ts
+++ src/components/about/about-page.ts
@@ -10,43 +10,51 @@ The [DRIVER+](https://www.driver-project.eu) project has received funding from t
 
 const md2 = `<h4 class="primary-text">Quick reference</h4>
 
-<h5 class="primary-text">Adding an event</h5>
-
-An event can be created by clicking the \`+ ADD A NEW EVENT\` button on top of the main menu.
+<h5 class="primary-text">Users – Login</h5>
+Everyone can be **visitor** of the L3, and is allowed to select and consult the events that have been published on the L3. In addition, one can registrate himself/herself as a new L3 user. After registration he/she will receive the rights from the **administrator** to login as a regular **user**. He/she can do so clicking on the little man on the top right side, and providing his/her username and password. Thus being able to create new events or to edit events he/she has created before. In some cases the **administrator** can also provide the rights to login as an **editor**. As an editor the user has the option to edit all published events.
 
 <h5 class="primary-text">Selecting an event</h5>
 
-An event can be selected in the main menu by clicking the button of the concerned event from the list of events that are present in the repository. To this purpose, also filtering options at the left can be used to reduce the list of available events. Filtering can be done by:
+An event can be selected in the main menu by clicking the button of the concerned event from the list of events that are present in the repository. To this purpose, also filtering options at the left can be used to reduce the list of available events. Filtering can be done:
 
-- Text of event names or their short descriptions
-- Countries
-- Types of event
-- Incidents (initial or secondary)
-- Crisis management functions for which lessons have been defined
+- By using the text filter by entering key words.
+- By selecting countries, types of event, incidents (initial or secondary), and/or crisis management functions.
 
-After having selected an event, the document is presented with the most important information that already has been provided on this event. By clicking the \`EDIT DOCUMENT\` button, more detailed information can be consulted (or edited).
+After having selected an event, the document is presented with the most important information that already has been provided on this event. If required, this Event document can be printed (Ctrl P).
 
-<h5 class="primary-text">Editing an event</h5>
+In case one is logged in as an editor, the \`EDIT DOCUMENT\` button appears on the top right. By clicking this button, more detailed information can be consulted.
 
-After having selected an event it can be edited and consulted. The Content menu at the left shows the various sections to describe and to characterise the event and lessons. These sections are:
+<h5 class="primary-text">Adding an event</h5>
 
-- **General event data:** Name and general description of the event, including time and place.
-- **Incident characteristics:** Description of the initial incident and other (cascading) incidents (if any) during the event, including their (potential) impact.
-- **Geographic characteristics:** Geographic dimension and scale of the incident(s), including a map.
-- **Involved organisations:** Organisations that were involved in performing crisis management functions during the event.
-- **Critical CM functions:** Most essential crisis management functions that had to be executed to handle the incident(s).
-- **Lessons:** Add and describe lessons for single crisis management functions; lessons are described and can be characterised by their technical or non-technical nature, and their potential contribution to improve crisis management. 
-- **Publications:** References to relevant documents about the event or lessons.
-- **Multimedia sources:** References to relevant all kinds of multimedia data.
-- **Editors:** Authors who contributed to the contents of the event.
+An event can be created by clicking the \`+ ADD A NEW EVENT\` button on top of the main menu. After having added an event it can be edited.
 
-<h5 class="primary-text">Showing, saving or deleting an event</h5>
+<h5 class="primary-text">Editing or consulting an event</h5>
 
-Below the Content menu there are three buttons:
+The Content menu at the left shows the various sections to describe and to characterise the event and lessons. These sections are:
+- General event data: Name and general description of the event, including time and place.
+- Incident characteristics: Description of the initial incident and other (cascading) incidents (if any) during the event, including their (potential) impact.
+- Geographic characteristics: Geographic dimension and scale of the incident(s), including a map.
+- Involved organisations: Organisations that were involved in performing crisis management functions during the event.
+- Critical CM functions: essential crisis management functions that had to be executed to handle the incident(s) and that were typical for the lessons to be learned from this incident.
+- Lessons: Add and describe lessons for single crisis management functions; lessons are described and can be characterised by their technical or non-technical nature, and their potential contribution to improve crisis management.
+- Publications: References to relevant documents about the event or lessons.
+- Multimedia sources: References to relevant all kinds of multimedia data.
+- Editors: Authors who contributed to the contents of the event.
+
+<h5 class="primary-text">Showing or deleting an event</h5>
+
+Below the Content menu there are two buttons:
+
 - \`SHOW EVENT\` to jump to the Event document
-- \`SAVE EVENT\` to save all entered (or updated) data
 - \`DELETE EVENT\` to delete the event from the repository
-`;
+
+<h5 class="primary-text">Additional editors of an event</h5>
+
+The editor of a new event can also provide rights to edit an event to other persons who have editors-rights. This can be done by providing their e-mail addresses on the bottom left.
+
+<h5 class="primary-text">Publishing an event</h5>
+
+In case one is the user or editor of an event, he/she can decide to publish the event in the L3 by checking the box next to \`PUBLISH EVENT\` at top left of the Event document screen. Hereafter the event is publicly available. By checking the box again the event will be unpublished.`;
 
 export const AboutPage = () => ({
   view: () =>
--- src/components/about/help-page.ts
+++ src/components/about/help-page.ts
@@ -5,28 +5,38 @@ import l3 from '../../assets/l3-schema.png';
 const md = `<h4 class="primary-text">Explanation of the Lessons Learned Library</h4>
 <h5 class="primary-text">Objective</h5>
 
-The Lessons Learned Library (L3) aims to support organisations in editing, maintaining, consulting and sharing lessons within the domain of crisis management (CM) and disaster risk reduction (DRR) in Europe. Thereby, sharing of lessons is not strictly limited to one organisation. L3 is especially intended to share lessons across organisations, across sectors, and across countries with the ultimate goal to improve CM and DRR in Europe by learning from each other’s experiences.
-Lessons may be collected from various types of events: routine, every day operations, crisis situations, training and exercises, experiments and tests, but also from risk management studies or preventive activities. Learning lessons can be considered as a structured approach to produce and apply experience-based knowledge to develop and improve doctrines, organisations, training, equipment, leadership, personnel and facilities to achieve more effective, efficient and safe operations.
+The Lessons Learned Library (L3) aims to support organisations in editing, maintaining, consulting and sharing lessons within the domain of crisis management (CM) and disaster risk reduction (DRR) in Europe. Thereby sharing of lessons is not strictly limited to one organisation. L3 is especially intended to share lessons across organisations, across sectors, and across countries with the ultimate goal to improve CM and DRR in Europe by learning from each other’s experiences.
 
-In simple terms, a lesson is a set of answers to questions such as: What was the situation?, What was the impact?, What went well in emergency management and is worthwhile to implement?, or What went wrong and which improvements are needed? To this purpose, users can provide precious information and lessons from events by adding a new event to the repository. By filling out a limited number of forms collected data and experiences can be shared with other emergency management communities in Europe.
+Lessons may be collected from various types of events: routine, every day operations, near incidents, crisis situations, training and exercises, experiments and tests, but also from risk management studies or other preventive activities. Learning lessons can be considered as a structured approach to produce and apply experience-based knowledge to develop and improve doctrines, organisations, training, equipment, leadership, personnel and facilities to achieve more effective, efficient and safe operations.
 
-Lessons may be of varying nature and of mixed interest outside the organisation or sector where they were collected. Therefore, a filtering mechanism enables users to select the kind of information they would like to consult: information about an event that took place (e.g. a Trial in the DRIVER+ project), lessons from a particular type of event (e.g. exercises), or from certain types of incidents (e.g. forest fires or bomb attacks), or from specific crisis management functions (e.g. evacuation or situation assessment).
+In simple terms, a lesson is a set of answers to questions such as: ‘What was the situation?’, ‘What was the impact?’, ‘What went well in emergency management and is worthwhile to implement?’, or ‘What went wrong and which improvements are needed?’. To this purpose, users can provide precious information and lessons from events by adding a new event to the repository. By filling out a limited number of forms collected data and experiences can be shared with other emergency management communities in Europe.
+
+Lessons may be of varying nature and of mixed interest outside the organisation or sector where they have been collected. Therefore, a filtering mechanism enables users to select the kind of information they would like to consult: information about an event that took place (e.g. a Trial in the DRIVER+ project), lessons from a particular type of event (e.g. exercises),  from certain types of incidents (e.g. forest fires or bomb attacks), or from specific crisis management functions (e.g. evacuation or situation assessment).
 
 <h5 class="primary-text">Functionalities and structure</h5>
 
-The main functionalities of the L3 are to add and edit crisis events and associated lessons from these events, and to find and consult specific events or lessons. Because the aim of the L3 is to share lessons across the CM community throughout Europe the user interface is in English. For the same reason users are requested (but are not obliged) to provide their information on events and lessons in English as well.
+The main functionalities of the L3 are to add and edit crisis events and associated lessons from these events, and to find and consult specific events or lessons. Because the aim of the L3 is to share lessons across the CM community throughout Europe the user interface is in English. For the same reason users are encouraged (but are not obliged) to provide their information on events and lessons in English as well.
 
-The L3 contains of a set of events and their associated lessons. As depicted in the adjacent figure, each event in the repository can contain 0, 1 or more lessons, while each lesson is linked to exactly one specific crisis management function. Please note that a certain function can be addressed by several lessons from one or more events.`;
+The L3 contains a set of events and their associated lessons. As depicted in the adjacent figure, each event in the repository can contain 0, 1 or more lessons, while each lesson in its turn can be linked to one or more crisis management functions. Note that a certain CM function can be addressed by different lessons from the same or from other events.`;
 
-const md2 = `An event is described by:
+const md2 = `<h5 class="primary-text">Events, lessons and CM functions</h5>
 
-- A summary, including a number of general data such as type of event (e.g. an incident or an exercise), and the date and place of the event;
--	More detailed information on the incident scenario and crisis management operations, such as the initial incident and cascading events, the (potential) impact, a map of the situation, involved organisations, and an overview of essential CM-functions that had to be executed;
+An **event** is described by:
+
+-	A summary, including some general data such as type of event (e.g. an incident or an exercise), and the date and place of the event.
+-	More detailed information on the incident scenario and crisis management operations, such as the initial incident and cascading events, the (potential) impact, a map of the situation, involved organisations, and an overview of critical CM functions that had to be executed.
 -	Lessons that have been learned from the event.
 
-A lesson consists of two sets of information. First of all, a description of the observation of positive or negative experiences concerning the applicable CM function during the event. This includes the performance of executing the CM function during the event, which is expressed in three criteria: the effectiveness of the CM function (quality), the efficiency of the CM function (cost aspects), and the risks to which responders have been exposed.
+A **lesson** consists of two sets of information:
 
-Secondly, the characterisation of a (potential) solution to improve the CM function based on the experiences during the event. This includes a description of the expected performance improvement of the CM function once it will have been implemented, as well as an indication of the impact reduction of the event’s incident.;`
+-	A description of the observation of positive or negative experiences concerning the applicable CM function during the event. This includes the performance of executing the CM function during the event, which is expressed by the effectiveness (adequacy) of executing the CM function on a 5 point-scale.
+-	The characterisation of a (potential) solution to improve the CM function based on the experiences during the event. This includes a description of the expected performance improvement of the CM function (also expressed on a 5 point-scale) once it will have been implemented. In addition an indication can be provided of the expected impact reduction by the solution. To this purpose five impact dimensions of UNISDR are used: numbers of victims, material damage, loss of services, social/economic losses, and environmental degradation.
+
+The L3 includes 24 **CM functions** to choose from. It concerns:
+
+-	_Operational CM functions_<br>Alerting (112), Crisis/Risk communication to society, Crowd management, Decontamination, Emergency Health Care, Evacuation & Shelter, Fight/Eliminate incident source (e.g. fire-fighting, stop a leakage), Law enforcement, Provide basic needs to the population, Remove debris, Rescue operations (SAR), and Restore critical services.
+-	_Operations enabling or supporting CM functions_<br>Command, Control and Coordination (C3)/Information management, Detection/Surveillance, International collaboration (incl. home nation support), Logistics/Resource management, Situation assessment, Social media mining, Traffic management, Up-scale/Down-scale of emergency services, and Volunteer management.
+-	_Preparatory CM functions_<br>Education & Training, Planning/Doctrine development, and Risk assessment.`;
 
 export const HelpPage = () => ({
   view: () =>
