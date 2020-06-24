3ca8008cf7b8c0647e35700bf2f1aa438a2df054
Erik Vullings <erik.vullings@gmail.com>
2020-06-24 11:28:24 +0200

    Updated logo, favicon, deps

--- src/components/home/home-page.ts
+++ src/components/home/home-page.ts
@@ -1,79 +1,83 @@
-import m from 'mithril';
-import { Button, Icon, Parallax } from 'mithril-materialized';
-import { SlimdownView } from 'mithril-ui-form';
-import background from '../../assets/background.jpg';
-import driverLogo from '../../assets/driver-logo-compact2.png';
-import euLogo from '../../assets/eu-flag-logo.png';
-import logo from '../../assets/logo_white.svg';
-import { Dashboards, dashboardSvc } from '../../services/dashboard-service';
+import m from "mithril";
+import { Button, Icon, Parallax } from "mithril-materialized";
+import { SlimdownView } from "mithril-ui-form";
+import background from "../../assets/background.jpg";
+import driverLogo from "../../assets/driver-logo-compact2.png";
+import euLogo from "../../assets/eu-flag-logo.png";
+import logo from "../../assets/logo_white.svg";
+import { Dashboards, dashboardSvc } from "../../services/dashboard-service";
 
 export const HomePage = () => ({
   view: () => [
-    m('.row', [
+    m(".row", [
       m(
-        'nav.yellow.darken-3',
-        m('.nav-wrapper', [
+        "nav.yellow.darken-3",
+        m(".nav-wrapper", [
           m(
-            'a.brand-logo[href=#]',
-            { style: 'margin: 0 10px 0 20px; left: 20px' },
-            m(`img[width=48][height=48][src=${logo}]`, { style: 'margin-top: 5px; margin-left: -5px;' })
+            "a.brand-logo[href=#]",
+            { style: "margin: 0 10px 0 20px; left: 20px" },
+            m(`img[width=125][height=125][src=${logo}]`, {
+              style: "margin-top: -30px; margin-left: -50px;",
+            })
           ),
           m(
-            'h3.center.yellow.darken-3.hide-on-small-only',
-            { style: 'margin: 0 auto; padding: 10px 0;' },
-            'Lessons Learned Library'
+            "h3.center.yellow.darken-3.hide-on-small-only",
+            { style: "margin: 0 auto; padding: 10px 0;" },
+            "Lessons Learned Library"
           ),
         ]),
         m(
-          '.overlay.center',
+          ".overlay.center",
           {
-            style: 'position: relative; top: 350px;',
+            style: "position: relative; top: 350px;",
           },
           m(Button, {
-            className: 'yellow darken-3 btn-large',
-            label: 'Get started',
+            className: "yellow darken-3 btn-large",
+            label: "Get started",
             onclick: () => dashboardSvc.switchTo(Dashboards.SEARCH),
           })
         )
       ),
       m(Parallax, { src: background }),
       m(
-        '.section.white',
-        m('.row.container.center', [
+        ".section.white",
+        m(".row.container.center", [
           m(SlimdownView, {
-            md:
-              `## Learn and share
+            md: `## Learn and share
 
               The **Lessons Learned Library (L3)** is a repository in the domain of Crisis Management (CM) and Disaster Risk Reduction (DRR) for collecting and sharing lessons from preventive or response activities at events such as severe incidents, crisis situations, tests or exercises.`,
           }),
-          m('.row', [
+          m(".row", [
             m(
-              '.col.s12.m4',
-              m('.icon-block', [
-                m('.center', m(Icon, { iconName: 'group' })),
-                m('h5.center', 'Share your experience'),
-                m('p.light', 'Help others, so they can take advantage of your experiences.'),
-              ])
-            ),
-            m(
-              '.col.s12.m4',
-              m('.icon-block', [
-                m('.center', m(Icon, { iconName: 'flash_on' })),
-                m('h5.center', 'Quick and easy'),
+              ".col.s12.m4",
+              m(".icon-block", [
+                m(".center", m(Icon, { iconName: "group" })),
+                m("h5.center", "Share your experience"),
                 m(
-                  'p.light',
-                  'In a short period of time you can edit information and key lessons from an event in a well-organised way.'
+                  "p.light",
+                  "Help others, so they can take advantage of your experiences."
                 ),
               ])
             ),
             m(
-              '.col.s12.m4',
-              m('.icon-block', [
-                m('.center', m(Icon, { iconName: 'public' })),
-                m('h5.center', 'Open to any professional'),
+              ".col.s12.m4",
+              m(".icon-block", [
+                m(".center", m(Icon, { iconName: "flash_on" })),
+                m("h5.center", "Quick and easy"),
                 m(
-                  'p.light',
-                  'Not locked away in a safe or hidden in some report, but open to any CM professional interested to learn from other colleagues.'
+                  "p.light",
+                  "In a short period of time you can edit information and key lessons from an event in a well-organised way."
+                ),
+              ])
+            ),
+            m(
+              ".col.s12.m4",
+              m(".icon-block", [
+                m(".center", m(Icon, { iconName: "public" })),
+                m("h5.center", "Open to any professional"),
+                m(
+                  "p.light",
+                  "Not locked away in a safe or hidden in some report, but open to any CM professional interested to learn from other colleagues."
                 ),
               ])
             ),
@@ -82,29 +86,43 @@ export const HomePage = () => ({
       ),
     ]),
     m(
-      'footer.page-footer.yellow.darken-3',
-      { style: 'height: 100px; padding: 5px 0;' },
+      "footer.page-footer.yellow.darken-3",
+      { style: "height: 100px; padding: 5px 0;" },
       m(
-        '.container',
-        m('.clearfix', [
-          m('div', { style: 'float: left; margin-right: 10px;' }, [
-            m('img', { src: euLogo, width: 100, height: 67, style: 'display: block; margin-left: 20px;' }),
-            m('span', 'v1.0, September 2019'),
+        ".container",
+        m(".clearfix", [
+          m("div", { style: "float: left; margin-right: 10px;" }, [
+            m("img", {
+              src: euLogo,
+              width: 100,
+              height: 67,
+              style: "display: block; margin-left: 20px;",
+            }),
+            m("span", "v1.0, September 2019"),
           ]),
-          m('div', { style: 'float: right; margin-left: 10px;' }, [
-            m('img', { src: driverLogo, width: 67, height: 67, style: 'display: block; margin-left: 40px;' }),
+          m("div", { style: "float: right; margin-left: 10px;" }, [
+            m("img", {
+              src: driverLogo,
+              width: 67,
+              height: 67,
+              style: "display: block; margin-left: 40px;",
+            }),
             m(
-              'a.primary-text',
-              { style: 'display: block', href: 'https://www.project-driver.eu', target: '_blank' },
-              'www.project-driver.eu'
+              "a.primary-text",
+              {
+                style: "display: block",
+                href: "https://www.project-driver.eu",
+                target: "_blank",
+              },
+              "www.project-driver.eu"
             ),
           ]),
           m(
-            '.white-text',
+            ".white-text",
             `This project has received funding from the European Union's 7th Framework Programme for Research,
           Technological Development and Demonstration under Grant Agreement (GA)`
           ),
-          m('span', '©DRIVER+'),
+          m("span", "©DRIVER+"),
         ])
       )
     ),
--- src/components/layout.ts
+++ src/components/layout.ts
@@ -1,52 +1,67 @@
-import m, { Vnode } from 'mithril';
-import { Icon } from 'mithril-materialized';
-import logo from '../assets/logo_white.svg';
-import { IDashboard } from '../models';
-import { dashboardSvc } from '../services/dashboard-service';
+import m, { Vnode } from "mithril";
+import { Icon } from "mithril-materialized";
+import logo from "../assets/logo_white.svg";
+import { IDashboard } from "../models";
+import { dashboardSvc } from "../services/dashboard-service";
 
-const stripRouteParams = (path: string) => path.replace(/:[a-zA-Z]+/, '');
+const stripRouteParams = (path: string) => path.replace(/:[a-zA-Z]+/, "");
 
 const isActiveRoute = (route = m.route.get()) => (path: string) =>
-  path.length > 1 && route.indexOf(stripRouteParams(path)) >= 0 ? '.active' : '';
+  path.length > 1 && route.indexOf(stripRouteParams(path)) >= 0
+    ? ".active"
+    : "";
 
 export const Layout = () => ({
   view: (vnode: Vnode) => {
     const isActive = isActiveRoute();
-    return m('.main', [
+    return m(".main", [
       m(
-        '.navbar-fixed',
-        { style: 'z-index: 1001' },
+        ".navbar-fixed",
+        { style: "z-index: 1001" },
         m(
-          'nav',
-          m('.nav-wrapper', [
-            m('a.brand-logo[href=#].hide-on-small-and-down', { style: 'margin-left: 20px' }, [
-              m(`img[width=48][height=48][src=${logo}]`, { style: 'margin-top: 5px; margin-left: -5px;' }),
-              m(
-                'div',
-                { style: 'margin-top: 0px; position: absolute; top: 16px; left: 50px; width: 400px;' },
+          "nav",
+          m(".nav-wrapper", [
+            m(
+              "a.brand-logo[href=#].hide-on-small-and-down",
+              { style: "margin-left: 20px" },
+              [
+                m(`img[width=125][height=125][src=${logo}]`, {
+                  style: "margin-top: -30px; margin-left: -30px;",
+                }),
                 m(
-                  'h4.center.yellow.darken-3.hide-on-med-and-down',
-                  { style: 'text-align: left; margin: 0;' },
-                  'Lessons Learned Library'
-                )
-              ),
-            ]),
+                  "div",
+                  {
+                    style:
+                      "margin-top: 0px; position: absolute; top: 16px; left: 50px; width: 400px;",
+                  },
+                  m(
+                    "h4.center.yellow.darken-3.hide-on-med-and-down",
+                    { style: "margin: 0;" },
+                    "Lessons Learned Library"
+                  )
+                ),
+              ]
+            ),
             m(
               // 'a.sidenav-trigger[href=#!/home][data-target=slide-out]',
               // { onclick: (e: UIEvent) => e.preventDefault() },
               m.route.Link,
-              { className: 'sidenav-trigger', 'data-target': 'slide-out', href: m.route.get() },
+              {
+                className: "sidenav-trigger",
+                "data-target": "slide-out",
+                href: m.route.get(),
+              },
               m(Icon, {
-                iconName: 'menu',
-                className: '.hide-on-med-and-up',
-                style: 'margin-left: 5px;',
+                iconName: "menu",
+                className: ".hide-on-med-and-up",
+                style: "margin-left: 5px;",
               })
             ),
             m(
-              'ul.right',
+              "ul.right",
               dashboardSvc
                 .getList()
-                .filter(d => d.visible || isActive(d.route))
+                .filter((d) => d.visible || isActive(d.route))
                 .map((d: IDashboard) =>
                   m(
                     `li${isActive(d.route)}`,
@@ -54,9 +69,12 @@ export const Layout = () => ({
                       m.route.Link,
                       { href: d.route },
                       m(
-                        'span',
+                        "span",
                         d.icon
-                          ? m('i.material-icons.white-text', typeof d.icon === 'string' ? d.icon : d.icon())
+                          ? m(
+                              "i.material-icons.white-text",
+                              typeof d.icon === "string" ? d.icon : d.icon()
+                            )
                           : d.title
                       )
                     )
@@ -66,7 +84,7 @@ export const Layout = () => ({
           ])
         )
       ),
-      m('.container', vnode.children),
+      m(".container", vnode.children),
     ]);
   },
 });
