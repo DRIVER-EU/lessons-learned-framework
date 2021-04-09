20a28b451abd7adcaaacbedf3c6fa34e0c40b192
Erik Vullings <erik.vullings@gmail.com>
2021-04-09 10:52:43 +0200

    Specify leaflet

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
-import 'material-icons/iconfont/material-icons.css';
-import 'materialize-css/dist/css/materialize.min.css';
-import m from 'mithril';
-import './css/style.css';
-import { dashboardSvc } from './services/dashboard-service';
-import { Auth } from './services/login-service';
+import "material-icons/iconfont/material-icons.css";
+import "materialize-css/dist/css/materialize.min.css";
+import m from "mithril";
+import "./css/style.css";
+import { dashboardSvc } from "./services/dashboard-service";
+import { Auth } from "./services/login-service";
+import L from "leaflet";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("url:leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("url:leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("url:leaflet/dist/images/marker-shadow.png"),
+});
 
 const restoreSession = async () => {
   await Auth.refreshLogin();
